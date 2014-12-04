63527a40dad102df4ef9dfe54a7e25d8024a0246
coul0127 <coul0127@algonquinlive.com>
2014-12-04 15:48:34 -0500

    photo album page added

--- plugins/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Jessica\MAD9135\photoqr-master\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\Jessica\MAD9135\photoqr-master\platforms\android\AndroidManifest.xml \
--- plugins/platforms/android/ant-build/PhotoQR-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Jessica\MAD9135\photoqr-master\platforms\android\ant-build\PhotoQR-debug-unaligned.apk : \
-C:\Users\Jessica\MAD9135\photoqr-master\platforms\android\ant-build\PhotoQR.ap_ \
-C:\Users\Jessica\MAD9135\photoqr-master\platforms\android\ant-build\classes.dex \
-C:\Users\Jessica\MAD9135\photoqr-master\platforms\android\CordovaLib\ant-build\classes.jar \
-C:\Users\Jessica\MAD9135\photoqr-master\platforms\android\libs\com.google.zxing.client.android.captureactivity.jar \
