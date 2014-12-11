c42632148087055502b3b0195b69ddb224fe1db8
ham-and-cheese-sandwhich <Password1!>
2014-12-11 17:37:02 -0500

    final

---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,5 @@
+c:\wamp\www\mad9135\photoQR\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+c:\wamp\www\mad9135\photoQR\platforms\android\ant-build\CordovaApp.ap_ \
+c:\wamp\www\mad9135\photoQR\platforms\android\ant-build\classes.dex \
+C:\wamp\www\mad9135\photoQR\platforms\android\CordovaLib\ant-build\classes.jar \
+c:\wamp\www\mad9135\photoQR\platforms\android\libs\com.google.zxing.client.android.captureactivity.jar \
