c11c7427c52302390fbb6c1dc50758359c5e15b1
ham-and-cheese-sandwhich <Password1!>
2014-12-11 19:12:16 -0500

    added ios

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-c:\wamp\www\mad9135\photoQR\platforms\android\ant-build\AndroidManifest.xml : \
-c:\wamp\www\mad9135\photoQR\platforms\android\AndroidManifest.xml \
+C:\wamp\www\mad9135\photoQR\platforms\android\ant-build\AndroidManifest.xml : \
+C:\wamp\www\mad9135\photoQR\platforms\android\AndroidManifest.xml \
