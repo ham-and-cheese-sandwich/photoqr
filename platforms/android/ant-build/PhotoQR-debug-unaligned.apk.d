bdcf3fc8ed45841c89cc586c2e8878e0bd6bbc50
ham-and-cheese-sandwhich <Password1!>
2014-12-11 19:22:37 -0500

    config modified

--- platforms/android/ant-build/PhotoQR-debug-unaligned.apk.d
+++ platforms/android/ant-build/PhotoQR-debug-unaligned.apk.d
@@ -1,5 +1,5 @@
-C:\wamp\www\mad9135\photoQR\platforms\android\ant-build\PhotoQR-debug-unaligned.apk : \
-C:\wamp\www\mad9135\photoQR\platforms\android\ant-build\PhotoQR.ap_ \
-C:\wamp\www\mad9135\photoQR\platforms\android\ant-build\classes.dex \
+c:\wamp\www\mad9135\photoQR\platforms\android\ant-build\PhotoQR-debug-unaligned.apk : \
+c:\wamp\www\mad9135\photoQR\platforms\android\ant-build\PhotoQR.ap_ \
+c:\wamp\www\mad9135\photoQR\platforms\android\ant-build\classes.dex \
 C:\wamp\www\mad9135\photoQR\platforms\android\CordovaLib\ant-build\classes.jar \
-C:\wamp\www\mad9135\photoQR\platforms\android\libs\com.google.zxing.client.android.captureactivity.jar \
+c:\wamp\www\mad9135\photoQR\platforms\android\libs\com.google.zxing.client.android.captureactivity.jar \
